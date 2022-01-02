2f2249330f941c6ee22738ae2a5b5679ebd89980
alevans99 <alevans99@gmail.com>
2022-01-02 13:11:34 +0000

    Adds Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
-
 import SearchPage from "./components/pages/SearchPage";
 import ShowPage from "./components/pages/ShowPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import Header from "./components/sub-components/Header";
 
 function App() {
   const [showsDisplayed, setShowsDisplayed] = useState([]);
   const [firstLoad, setFirstLoad] = useState(true)
-
+  
   return (
     <BrowserRouter>
       <div className="App">
+        <Header></Header>
         <Routes>
           <Route path="/" element={<SearchPage showsDisplayed={showsDisplayed} setShowsDisplayed={setShowsDisplayed} firstLoad={firstLoad} setFirstLoad={setFirstLoad}/>} />
           <Route path="/show" element={<ShowPage />} />
---
+++ src/components/sub-components/Header.js
@@ -0,0 +1,13 @@
+import '../../styles/Header.css';
+
+
+function Header() {
+  return (
+    <header className="Header">
+        <h1>Worth Starting?</h1>
+
+    </header>
+  );
+}
+
+export default Header;
