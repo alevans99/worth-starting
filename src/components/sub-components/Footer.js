e6d2b8742a503446c425ee488343f00695d08cb4
alevans99 <alevans99@gmail.com>
2022-01-04 21:20:50 +0000

    Updates attribution

--- src/components/sub-components/Footer.js
+++ src/components/sub-components/Footer.js
@@ -4,7 +4,7 @@ import '../../styles/Footer.css';
 function Footer() {
   return (
     <footer className="Footer">
-       <p>Images from <a href='https://www.unsplash.com'>Unsplash</a></p>
+       <p>Image from <a href='https://pixabay.com'>Pixabay</a></p>
        <p>Icons from <a href='https://material.io'>Material.io</a></p>
        <p>API data from <a href='https://www.tvmaze.com/api'>TV Maze</a></p>
     </footer>
